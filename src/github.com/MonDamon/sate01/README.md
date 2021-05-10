5002d2305e511c7b5a50aa61f029f11a70ca1c29
Mondamon <souza.cleyton@gmail.com>
2021-05-10 16:53:42 -0300

    commit inicial

---
+++ src/github.com/MonDamon/sate01/README.md
@@ -0,0 +1,3 @@
+# sate01
+
+Golang app created with generator-go and yeoman.
---
+++ src/github.com/MonDamon/sate01/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/MonDamon/sate01/hello"
+)
+
+func main() {
+	fmt.Println(hello.BuildHello())
+}
